2d33b2160b72830004251ddcb755dc064853af84
JanCimWay <janis.cimermanis@gmail.com>
2024-02-18 12:10:07 +0000

    Add update surplus sheet function

--- run.py
+++ run.py
@@ -62,6 +62,16 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided.
+    """
+    print("Updating surplus worksheet ...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -76,7 +86,7 @@ def calculate_surplus_data(sales_row):
     stock_row = stock[-1]
     
     surplus_data = []
-    # with zip method we can worj with 2 lists at the same time
+    # with zip method we can work with 2 lists at the same time
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
@@ -92,7 +102,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to lLove Sandwitches Data Automation")
 main()
